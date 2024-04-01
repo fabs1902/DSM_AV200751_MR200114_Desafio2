e5c8b5c038fafb4c363f733513325a10e8f01aa3
fabs1902 <fabiolamartinez190201@gmail.com>
2024-04-01 11:17:06 -0600

    Adding app

---
+++ app/src/main/java/com/example/dsm_av200751_mr200114_desafio2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dsm_av200751_mr200114_desafio2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DSM_AV200751_MR200114_Desafio2"
+include(":app")
