347f69f237444f25d5e1a078cd5be85d29f9d425
Araniva27 <manuelaraniva07@gmail.com>
2024-04-01 22:58:26 -0600

    creacion de actividad para registrar usuarios

--- app/src/main/java/com/example/dsm_av200751_mr200114_desafio2/MainActivity.kt
+++ app/src/main/java/com/example/dsm_av200751_mr200114_desafio2/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.dsm_av200751_mr200114_desafio2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var botonRegistrar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botonRegistrar = findViewById(R.id.btnRegistrarse)
+
+        botonRegistrar.setOnClickListener {
+            val intent = Intent(this, RegistroUsuarios::class.java)
+            startActivity(intent)
+        }
     }
 }
